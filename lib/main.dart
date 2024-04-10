0530c4c02cd5f1f5df96aa0a483781f3aee424cf
princekae <pkebayzim@gmail.com>
2024-04-10 22:05:14 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:food/screens/categories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 78, 222, 210),
+  ),
+  textTheme: GoogleFonts.poppinsTextTheme(),
+);
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
